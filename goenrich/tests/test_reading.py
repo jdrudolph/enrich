68d355e642f67d1c46ba7ca6dfafafab2917cb9c
Saulius Lukauskas <luksaulius@gmail.com>
2016-09-27 13:52:53 +0100

    added tests for input with file objects

--- goenrich/tests/test_reading.py
+++ goenrich/tests/test_reading.py
@@ -1,16 +1,28 @@
 import unittest
-
+import gzip
 import goenrich
 
 class TestRead(unittest.TestCase):
     def test_ontology(self):
         G = goenrich.obo.ontology('db/go-basic.obo')
 
+    def test_ontology_from_file_obj(self):
+        with open('db/go-basic.obo') as f:
+            G = goenrich.obo.ontology(f)
+
     def test_goa(self):
         background = goenrich.read.goa('db/gene_association.goa_ref_human.gz')
 
+    def test_goa_from_file_obj(self):
+        with gzip.GzipFile('db/gene_association.goa_ref_human.gz') as f:
+            background = goenrich.read.goa(f)
+
     def test_gene2go(self):
         background = goenrich.read.gene2go('db/gene2go.gz')
+
+    def test_gene2go_from_file_obj(self):
+        with gzip.GzipFile('db/gene2go.gz') as f:
+            background = goenrich.read.gene2go(f)
 
 if __name__ == '__main__':
     unittest.main()
