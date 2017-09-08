37812eb416fb6ded2da717bd935913f47ec40f0e
Alexander Lenail <zfrenchee@gmail.com>
2017-09-08 14:25:06 -0400

    add a correctness test

--- goenrich/tests/test_enrichment.py
+++ goenrich/tests/test_enrichment.py
@@ -20,5 +20,16 @@ class TestGoenrich(unittest.TestCase):
             goenrich.enrich.analyze(O, query, background_attribute)
             print('pygraphviz could not be imported')
 
+    def test_pval_correctness(self):
+        O = goenrich.obo.ontology('db/go-basic.obo')
+        gene2go = goenrich.read.gene2go('db/gene2go.gz')
+        values = {k: set(v) for k,v in gene2go.groupby('GO_ID')['GeneID']}
+        background_attribute = 'gene2go'
+        goenrich.enrich.propagate(O, values, background_attribute)
+        query = gene2go['GeneID'].unique()[:20]
+        df = goenrich.enrich.analyze(O, query, background_attribute)
+        best_pval = float(df.dropna().sort_values('p').head(1).p)
+        assert pval == float(9.155001707860028e-08)
+
 if __name__ == '__main__':
     unittest.main()
